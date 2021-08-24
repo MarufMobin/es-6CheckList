6c0ce354f425e8d5b91a3d34c890997358c1be5b
unknown <mobinh264@gmail.com>
2021-08-25 01:45:14 +0600

    Fist task Done

---
+++ task-one.js
@@ -0,0 +1,9 @@
+// ১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো।
+
+const myName = "MD: Maruf Hussion";
+let myAge = 21;
+myAge = 22;
+
+
+// let const ==> ai duitai holo variable ja Es - 6 ar ar modhe 90 / 95% const use hoi ar ai const mane hoche j aita akbar declear hoye gele ar value aber assing kra jai na ata constant 
+// ar Let hoche j somosto value reAssing krte hoi oooi somosto khatre let bebohar hoi jmon boyos protibochor bare , mobile phone ar brand , bus ar uthar khatre same bus a uthina change o hote pare ai somosto kahtre let use hoi 
